9485a4edbe8a3829d9430575009868ff4a900e70
Sm1te <67575139+Sm1te@users.noreply.github.com>
2021-04-07 23:26:12 +0800

    Update ArrayDeque.java

--- ArrayDeque.java
+++ ArrayDeque.java
@@ -1,93 +1,101 @@
-public class ArrayDeque<T> {
-    private T[] items;
-    private int size;
-    private int nextFirst = 3;
-    private int nextLast = 4;
+public class ArrayDeque<T>{
 
-    public ArrayDeque(){
-        items = (T[]) new Object[8];
-        size = 0;
-    }
+	private T[] items;
+	private int size;
+	private int nextFirst = 7;
+	private int nextLast = 0;
 
-    public int size(){
-        return size;
-    }
 
-    public boolean isEmpty(){
-        return(size == 0);
-    }
+	public ArrayDeque() {
+		items = (T[])new Object[8];
+		size = 0;
+	}
 
-    public T get(int index){
-        return items[index];
-    }
+	public T get(int index){
+		return items[index];
+	}
 
-    public void printDeque(){
-        for(T item:items){
-            System.out.print(item + " ");
-        }
-    }
+	public int size(){
+		return size;
+	}
 
-    public void resize(){
-        T[] revisedone = (T[]) new Object[items.length * 4];
-        System.arraycopy(items, 0, revisedone, (items.length*2)-(items.length/2), items.length);//calculate the position of nextFirst;
-        nextFirst = items.length *3/2 -1;
-        nextLast = items.length *5/2 +1;
-    }
+	public boolean isEmpty(){
+		return (size==0);
+	}
 
-    public ArrayDeque(ArrayDeque other){
+	public void printDeque(){
+		for(T item: items){
+			System.out.print(item + " ");
+		}
+	}
 
-    }
+	private void resize(){
+		T[] resizedArray = (T[])new Object[items.length * 2];
+		System.arraycopy(items, 0, resizedArray, 0, nextFirst + 1);
+		if((nextFirst + 1) != items.length){
+			System.arraycopy(items, nextFirst + 1, resizedArray, items.length + nextFirst + 1, items.length - nextFirst - 1);
+		}
+		items = resizedArray;
+		nextFirst = nextFirst + resizedArray.length / 2;
+	}	
 
-    public void addFirst(T item){
-        if(nextFirst == -1){
-            nextFirst = items.length -1;
-        }
-        if(nextFirst == nextLast){
-            resize();
-        }
-        items[nextFirst] = item;
-        nextFirst--;
-        size++;
-    }
+	public void addFirst(T x){
+		if(nextFirst == -1){
+			nextFirst = items.length - 1;
+		}
 
-    public void addLast(T item){
-        if(nextLast == items.length){
-            nextLast = 0;
-        }
-        if(nextFirst == nextLast){
-            resize();
-        }
-        items[nextLast] = item;
-        nextLast++;
-        size++;
-    }
-    
-    public T removeFirst(){
-        if(isEmpty()){
-            System.out.println("Deque is empty");
-            return null;
-        }
-        if(nextFirst >= items.length){
-            nextFirst = 0;
-        }
-        T result = items[nextFirst+1];
-        items[nextFirst+1] = null;
-        nextFirst++;
-        size--;
-        return result;
-    }
-    public T removeLast(){
-        if(isEmpty()){
-            System.out.println("Deque is empty");
-            return null;
-        }
-        if(nextLast== -1){
-            nextLast = items.length - 1;
-        }
-        T result = items[nextLast - 1];
-        items[nextLast - 1] = null;
-        nextLast--;
-        size--;
-        return result;
-    } 
+		if(nextFirst == nextLast){
+			resize();
+		}
+
+		items[nextFirst] = x;
+		nextFirst = nextFirst - 1;
+		size += 1;
+	}
+
+	public void addLast(T x){
+
+		if(nextLast == items.length){
+			nextLast = 0;
+		}
+
+		if(nextFirst == nextLast){
+			resize();
+		}
+
+		items[nextLast] = x;
+		nextLast = nextLast + 1;
+		size += 1;
+	}
+
+	public T removeFirst(){
+		if(isEmpty()){
+			System.out.println("Deque is empty");
+			return null;
+		}
+		if(nextFirst >= items.length){
+			nextFirst = 0;
+		}
+		T result = items[nextFirst + 1];
+		items[nextFirst + 1] = null;
+		nextFirst += 1;
+		size = size - 1;
+		return result;
+	}
+
+	public T removeLast(){
+		if(isEmpty()){
+			System.out.println("Deque is empty");
+			return null;
+		}
+		if(nextLast < 0){
+			nextLast = items.length - 1;
+		}
+		T result = items[nextLast - 1];
+		items[nextLast - 1] = null;
+		nextLast -= 1;
+		size = size - 1;
+		return (T)result;
+	}
+
 }
